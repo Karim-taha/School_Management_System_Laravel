c796c82cdda16b31d5101945271965be9c77eab1
karim-taha <karimtaha111@yahoo.com>
2022-10-04 10:30:55 +0200

    Make Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use \App\Http\Controllers\ClassesController;
+use \App\Http\Controllers\GradeController;
+use \App\Http\Controllers\LevelController;
+use \App\Http\Controllers\DayController;
+use \App\Http\Controllers\TimeController;
+use \App\Http\Controllers\ShiftController;
+use \App\Http\Controllers\SubjectController;
+use \App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +27,16 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
+Route::prefix()->group(function() {
+    Route::resource('classes', ClassesController::class);
+    Route::resource('grades', GradeController::class);
+    Route::resource('levels', LevelController::class);
+    Route::resource('days', DayController::class);
+    Route::resource('times', TimeController::class);
+    Route::resource('shifts', ShiftController::class);
+    Route::resource('subjects', SubjectController::class);
+    Route::resource('sessions', SessionController::class);
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
